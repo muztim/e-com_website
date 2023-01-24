eeb825308ea0a15dac587322965e9a4fe6abcb29
Timothee Muzaliwa <tmuzaliwwa@gmail.com>
2023-01-24 01:09:18 +0100

    test changes

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     # return render_template('index.html')
-    return "This is an example app"
+    return "Hello world"
 
 
 if __name__ == "__main__":
